10ef95f3dbb5284221c0ba4cc79508dc7f4b8900
balvarez <bastianalvarez113@gmail.com>
2017-08-10 09:36:20 -0400

    cambios pantallas

--- app/src/main/java/app/com/balvarez/entregacargamovil/MainFirma.java
+++ app/src/main/java/app/com/balvarez/entregacargamovil/MainFirma.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainFirma extends AppCompatActivity {
-//hola84789494
+//hola84789494145569456954
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
