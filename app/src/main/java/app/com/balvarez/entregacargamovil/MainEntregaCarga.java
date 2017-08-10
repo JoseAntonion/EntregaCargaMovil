7c0b1c570e7bdf18e00ae13289d0fc4db8f2fd18
JoseAntonion <jose.venegas1411@gmail.com>
2017-08-10 10:42:50 -0400

    cambios jose

--- app/src/main/java/app/com/balvarez/entregacargamovil/MainEntregaCarga.java
+++ app/src/main/java/app/com/balvarez/entregacargamovil/MainEntregaCarga.java
@@ -1,13 +1,45 @@
 package app.com.balvarez.entregacargamovil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainEntregaCarga extends AppCompatActivity {
+public class MainEntregaCarga extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_entrega_carga;
+    private Button btn_no_entrega_carga;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_entrega_carga);
+        btn_entrega_carga = (Button) findViewById(R.id.btnEntregaCarga);
+        btn_entrega_carga.setOnClickListener(this);
+        btn_no_entrega_carga = (Button) findViewById(R.id.btnNoEntregaCarga);
+        btn_no_entrega_carga.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.btnEntregaCarga: {
+                Intent intent = new Intent(MainEntregaCarga.this, MainFirma.class);
+                startActivity(intent);
+                finish();
+                System.gc();
+                finish();
+            }
+            case R.id.btnNoEntregaCarga: {
+                Intent intent = new Intent(MainEntregaCarga.this, MainMotivoNoEntrega.class);
+                startActivity(intent);
+                finish();
+                System.gc();
+                finish();
+            }
+
+        }
     }
 }
--- app/src/main/java/app/com/balvarez/entregacargamovil/MainFirma.java
+++ app/src/main/java/app/com/balvarez/entregacargamovil/MainFirma.java
@@ -1,13 +1,45 @@
 package app.com.balvarez.entregacargamovil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainFirma extends AppCompatActivity {
+public class MainFirma extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_finalizar;
+    private Button btn_limpiar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_firma);
+        btn_finalizar = (Button) findViewById(R.id.btnFinalizar);
+        btn_finalizar.setOnClickListener(this);
+        btn_limpiar = (Button) findViewById(R.id.btnLimpiar);
+        btn_limpiar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.btnFinalizar: {
+                Intent intent = new Intent(MainFirma.this, MainODT.class);
+                startActivity(intent);
+                finish();
+                System.gc();
+                finish();
+            }
+            case R.id.btnLimpiar: {
+                Intent intent = new Intent(MainFirma.this, MainFirma.class);
+                startActivity(intent);
+                finish();
+                System.gc();
+                finish();
+            }
+
+        }
     }
 }
