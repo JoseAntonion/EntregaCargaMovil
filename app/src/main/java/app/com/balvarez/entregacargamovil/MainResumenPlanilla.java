76bd990caea67fc6b6cf6bde37381a23bdb813db
balvarez <bastianalvarez113@gmail.com>
2017-08-10 10:25:16 -0400

    cambios pantallas

--- app/src/main/java/app/com/balvarez/entregacargamovil/MainResumenPlanilla.java
+++ app/src/main/java/app/com/balvarez/entregacargamovil/MainResumenPlanilla.java
@@ -1,13 +1,51 @@
 package app.com.balvarez.entregacargamovil;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainResumenPlanilla extends AppCompatActivity {
-
+public class MainResumenPlanilla extends Activity implements View.OnClickListener {
+     private Button btn_ingresar;
+    private Button btn_Volver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_resumen_planilla);
+        btn_ingresar = (Button) findViewById(R.id.btnVolver);
+        btn_ingresar.setOnClickListener(this);
+        btn_Volver = (Button) findViewById(R.id.btnVolver);
+        btn_Volver.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.btnSiguiente: {
+
+                Intent intento = new Intent(MainResumenPlanilla.this, MainODT.class);
+                startActivity(intento);
+                finish();
+                System.gc();
+                finish();
+                break;
+            }
+            case R.id.btnVolver: {
+
+                Intent intento = new Intent(MainResumenPlanilla.this, MainEntregaCargaMovil.class);
+                startActivity(intento);
+                finish();
+                System.gc();
+                finish();
+                break;
+            }
+
+            default:
+                break;
+        }
+
     }
 }
